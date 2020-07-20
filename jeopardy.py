7292d06b07589ff5dafba75c42c9142165b324a7
Jesse Hall <jesse.hall@cooper.edu>
2020-07-20 10:10:06 -0400

    working with a test case question

--- jeopardy.py
+++ jeopardy.py
@@ -1,5 +1,23 @@
-import requests
+import requests, random, json
 
-response = requests.get('http://jservice.io/api/clues?category=139').json()
+#response = requests.get('http://jservice.io/api/clues?category=139').json()
+#response = requests.get('http://jservice.io/api/category?id=' + str(random.randint(0, 11500))).json()
 
-print(response)
+#for testing purposes
+response = requests.get('http://jservice.io/api/category?id=6783').json()
+print(type(response))
+#print(response)
+for i in response:
+    print(i)
+    print(response[i])
+
+print(json.dumps(response, indent= 4))
+holdquestion = response['clues'][0]
+print((holdquestion['question']))
+holdans = input('in the form of a question please answer the above ').capitalize()
+#print(response[0]['question'])
+if(holdans == holdquestion['answer']):
+    print('magic')
+else:
+    print('the answer was ' + holdquestion['answer'])
+
